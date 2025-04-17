17353732c351da5b8e137953ecd6cf0dedf38339
InnaBondarenko87 <141265858+InnaBondarenko87@users.noreply.github.com>
2025-04-17 18:03:15 +0000

    Deploying to gh-pages from @ InnaBondarenko87/goit-js-hw-11@14a97b24076a9a46a4838acf23362da1b723154f 🚀

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&c(s)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function c(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const i=document.querySelector(".form"),l=i.elements["search-text"];i.addEventListener("submit",async n=>{n.preventDefault();const r=l.value.trim();if(r===""){alert("Будь ласка, введіть пошукове слово!");return}try{const o=await axios.get(`https://api.example.com/search?q=${encodeURIComponent(r)}`);console.log("Результати пошуку:",o.data)}catch(o){console.error("Помилка при пошуку:",o)}});
+import{a as d,S as p,i as o}from"./assets/vendor-DEZpR2tN.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&c(n)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function c(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();const m="49743033-5279597e0b6160ec8a63a5c3f";function h(t){return d("https://pixabay.com/api/?",{params:{key:m,q:t,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:9}})}const u=document.querySelector(".gallery"),f=document.querySelector(".loader");let a=null;function g(t){const s=t.tags.split(",").slice(0,3).join(", ");return`
+    <li class="gallery-item">
+        <a href="${t.largeImageURL}" class="gallery-link">
+            <img
+                class="gallery-image"
+                src="${t.webformatURL}"
+                alt="${s}"
+            />
+            <div class="info">
+                <div class="info-item">
+                    <p class="info-label">Likes</p>
+                    <span class="info-value">${t.likes}</span>
+                </div>
+                <div class="info-item">
+                    <p class="info-label">Views</p>
+                    <span class="info-value">${t.views}</span>
+                </div>
+                <div class="info-item">
+                    <p class="info-label">Comments</p>
+                    <span class="info-value">${t.comments}</span>
+                </div>
+                <div class="info-item">
+                    <p class="info-label">Downloads</p>
+                    <span class="info-value">${t.downloads}</span>
+                </div>
+            </div>
+        </a>
+    </li>
+    `}function y(t){const s=t.map(g).join("");u.insertAdjacentHTML("beforeend",s),a?a.refresh():a=new p(".gallery a",{captionsData:"alt",captionDelay:250})}function v(){u.innerHTML="",a&&(a.destroy(),a=null)}function L(){f.classList.remove("hidden")}function b(){f.classList.add("hidden")}const w=document.querySelector(".form"),l=document.querySelector("input");w.addEventListener("submit",S);function S(t){t.preventDefault();const s=l.value.trim();if(!s){o.warning({position:"topRight",title:"Warning",message:"Please enter a search query"}),l.focus();return}L(),v(),h(s).then(i=>i.data).then(i=>{if(!i.hits||i.hits.length===0){o.warning({position:"topRight",title:"Warning",message:"Sorry, there are no images matching your search query. Please try again!"});return}y(i.hits)}).catch(i=>{o.error({position:"topRight",title:"Error",message:"Failed to fetch images. Please try again later."}),console.error("Error:",i)}).finally(()=>{b(),l.value=""})}
 //# sourceMappingURL=index.js.map
